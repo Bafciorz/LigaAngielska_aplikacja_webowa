69917431b25b7cea4af533ffa28de1b4739e0ddd
Bafciorz <jakub.niebafia@gmail.com>
2026-05-16 16:05:45 +0200

    stadiony

--- Liga.Web/Controllers/StadiumController.cs
+++ Liga.Web/Controllers/StadiumController.cs
@@ -1,6 +1,28 @@
-namespace Liga.Web.Controllers;
+using Liga.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class StadiumController
+namespace Liga.Web.Controllers;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Liga.Web.Models;
+
+public class StadiumsController:Controller
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public StadiumsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        if (HttpContext.Session.GetString("Username") == null)
+        {
+            return RedirectToAction("Logowanie", "Home");
+        }
+        
+        var Stadiums = _context.Stadiums.ToList();
+        return View(Stadiums);
+    }
 }
